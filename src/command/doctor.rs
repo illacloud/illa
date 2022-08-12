6087656089d0dd6cd940e53652cd17ac01c6fc4e
naj1n <dengzhanjiao@illasoft.com>
2022-08-12 16:15:10 +0800

    feat(subcommand: doctor): add test code

--- src/command/doctor.rs
+++ src/command/doctor.rs
@@ -2,6 +2,8 @@ use crate::{
     command::*,
     result::Result,
 };
+use bollard::{Docker, system::Version};
+
 
 // Executes the `illa doctor` command to 
 // check the prerequisites of self hosting
@@ -14,6 +16,11 @@ pub struct Cmd {
 impl Cmd {
     pub async fn run(&self) -> Result {
         println!("🛠 Checking the prerequisites for self hosting...");
+        let _docker = Docker::connect_with_local_defaults().unwrap();
+        match _docker.version().await {
+            Ok(version) => println!("docker version: {}", version.version.unwrap()),
+            Err(e) => println!("error")
+        }
         Ok(())
     }
 }
