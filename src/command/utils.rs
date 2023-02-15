fb35ce8c0b049b9bda30fc55ba7d038c68a7f0b3
Jay D <naj1n@mail.com>
2023-02-15 18:10:14 +0800

    feat(deploy): add flag --mount for subcommand `deploy`

--- src/command/utils.rs
+++ src/command/utils.rs
@@ -3,37 +3,40 @@ use anyhow::Ok;
 use std::{env, fs};
 
 #[cfg(target_os = "macos")]
-pub fn local_bind_init() -> String {
+pub fn local_bind_init(path: &String) -> String {
     use std::os::unix::fs::PermissionsExt;
-    fs::create_dir_all("/tmp/illa-data");
-    let attr = fs::metadata("/tmp/illa-data").unwrap();
+    fs::create_dir_all(path.clone());
+    let attr = fs::metadata(path.clone()).unwrap();
     let mut perms = attr.permissions();
     perms.set_mode(0o777);
-    fs::set_permissions("/tmp/illa-data", perms);
+    fs::set_permissions(path.clone(), perms);
 
-    String::from("/tmp/illa-data")
+    String::from(path)
 }
 
 #[cfg(target_os = "windows")]
-pub fn local_bind_init() -> String {
-    fs::create_dir_all("C:/temp/illa-data");
+pub fn local_bind_init(path: &String) -> String {
+    fs::create_dir_all(path.clone());
 
-    String::from("C:/temp/illa-data")
+    String::from(path)
 }
 
 #[cfg(target_os = "linux")]
-pub fn local_bind_init() -> String {
-    fs::create_dir_all("/tmp/illa-data");
+pub fn local_bind_init(path: &String) -> String {
+    fs::create_dir_all(path.clone());
 
-    String::from("/tmp/illa-data")
+    String::from(path)
 }
 
-pub fn local_bind_delete() -> Result {
-    if cfg!(windows) {
-        fs::remove_dir_all("C:/temp/illa-data");
-    } else {
-        fs::remove_dir_all("/tmp/illa-data");
-    }
+pub fn local_bind_delete(path: String) -> Result {
+    fs::remove_dir_all(path);
 
     Ok(())
+}
+
+pub fn get_default_mount() -> String {
+    let tmp_dir = env::temp_dir();
+    let temp_dir = tmp_dir.join("illa-builder");
+
+    temp_dir.display().to_string()
 }
